93ca91c24f2f612a754771b0c1ce030357b54588
kartic <kartic@gmail.com>
2016-05-04 15:02:58 +0100

    modified:   CourseNotes/main.tex 	modified:   CourseNotes/manifestationErrorRendering.tex 	modified:   PBRTOut/pngs/testmb_ball.scene_GRID_16_Samps_iter_1_1_uj.exr.png 	new file:   PBRTOut/pngs/testmb_ball.scene_GRID_2_Samps_iter_1_1_uj.exr.png 	modified:   PBRTOut/pngs/testmb_ball.scene_GRID_4_Samps_iter_1_1_uj.exr.png 	new file:   PBRTOut/pngs/testmb_ball.scene_GRID_64_Samps_iter_1_1_uj.exr.png 	modified:   PBRTOut/pngs/testmb_ball.scene_GRID_8_Samps_iter_1_1_uj.exr.png 	modified:   PBRTOut/pngs/testmb_ball.scene_LD_16_Samps_iter_1__uj.exr.png 	modified:   PBRTOut/pngs/testmb_ball.scene_LD_256_Samps_iter_1__uj.exr.png 	new file:   PBRTOut/pngs/testmb_ball.scene_LD_4096_Samps_iter_1__uj.exr.png 	new file:   PBRTOut/pngs/testmb_ball.scene_LD_4_Samps_iter_1__uj.exr.png 	modified:   PBRTOut/pngs/testmb_ball.scene_LD_64_Samps_iter_1__uj.exr.png 	modified:   PBRTOut/pngs/testmb_ball.scene_RAND_16_Samps_iter_1__uj.exr.png 	modified:   PBRTOut/pngs/testmb_ball.scene_RAND_256_Samps_iter_1__uj.exr.png 	new file:   PBRTOut/pngs/testmb_ball.scene_RAND_4096_Samps_iter_1__uj.exr.png 	new file:   PBRTOut/pngs/testmb_ball.scene_RAND_4_Samps_iter_1__uj.exr.png 	modified:   PBRTOut/pngs/testmb_ball.scene_RAND_64_Samps_iter_1__uj.exr.png 	new file:   PBRTOut/testmb_ball.scene_GRID_2_Samps_iter_1_1_uj.exr 	new file:   PBRTOut/testmb_ball.scene_GRID_64_Samps_iter_1_1_uj.exr 	new file:   PBRTOut/testmb_ball.scene_LD_4096_Samps_iter_1__uj.exr 	new file:   PBRTOut/testmb_ball.scene_LD_4_Samps_iter_1__uj.exr 	new file:   PBRTOut/testmb_ball.scene_RAND_4096_Samps_iter_1__uj.exr 	new file:   PBRTOut/testmb_ball.scene_RAND_4_Samps_iter_1__uj.exr 	modified:   Scenes/tmp_testmb_ball.scene.pbrt 	modified:   shell-scripts/RenderMB.sh

--- shell-scripts/RenderMB.sh
+++ shell-scripts/RenderMB.sh
@@ -6,11 +6,11 @@ beta=1
 
 for iter in `seq 1 $niter`;
 do
-    for ns in 8
+    for ns in 64
     do
 	nsq=$(expr $ns \* $ns)
-         #./RunPBRT.sh 0 LD $nsq $iter $pbrtScene
-         #./RunPBRT.sh 0 RAND $nsq $iter $pbrtScene
+         ./RunPBRT.sh 0 LD $nsq $iter $pbrtScene
+         ./RunPBRT.sh 0 RAND $nsq $iter $pbrtScene
          ./RunPBRT.sh 0 GRID $ns $iter $pbrtScene $beta
     done
 done
