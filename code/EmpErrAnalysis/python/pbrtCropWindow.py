af12fb09e7485c299eb41a308910292b644f18f5
Gurprit Singh <gsingh@cs.dartmouth.edu>
2016-08-11 13:20:24 -0400

    python script updated to remove name from pbrt file

--- code/EmpErrAnalysis/python/pbrtCropWindow.py
+++ code/EmpErrAnalysis/python/pbrtCropWindow.py
@@ -1,7 +1,12 @@
 import re
 import sys
+
+#input .pbrt file
 filename = sys.argv[1]
+
+#all the arguments containing crop window coordinates
 args = map(float, sys.argv[2:])
+#store crop window coordinates in simple variables
 x1 = sys.argv[2]
 x2 = sys.argv[3]
 y1 = sys.argv[4]
@@ -14,8 +19,11 @@ filedata = None
 with open(filename, 'r') as file :
     filedata = file.read()
 
-# Replace the target string
+# Replace the cropwindow with the input values in the .pbrt file
 filedata = re.sub(r'"float cropwindow".+',string, filedata)
+
+# Remove string with the filename in the .pbrt file
+filedata = re.sub(r'"string filename".+','', filedata)
 
 # Write the file out again
 with open(filename, 'w') as file:
